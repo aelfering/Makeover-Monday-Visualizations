cfee77a288514c917db3bfa29c448a40b5bdb260
Alex Elfering <anelfering@outlook.com>
2020-12-20 14:51:44 -0600

    added tidylog

--- 2020 W51 - Arsenal FC/Arsenal FC Script.R
+++ 2020 W51 - Arsenal FC/Arsenal FC Script.R
@@ -1,8 +1,17 @@
+
+list.of.packages <- c("tidyverse", 
+                      "ggplot2", 
+                      'ggrepel', 
+                      'extrafont', 
+                      'tidylog')
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
 library(tidyverse)
 library(ggplot2)
 library(ggrepel)
-loadfonts(device = "win")
 library(extrafont)
+library(tidylog)
 
 #font_import(paths = NULL, recursive = TRUE, prompt = TRUE,pattern = NULL)
 
